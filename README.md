aa9e68c23f5b38a1ddc70e07fd1387f56b8b51cc
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-10-07 11:26:35 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cutomizable Dashboard
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!-- resources/views/dashboard.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container font-figtree">
+    <div class="row">
+        <div class="col-md-3">
+            <!-- Card or widget 1 -->
+            <div class="card">
+                <div class="text-4xl">
+                    C1
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <!-- Card or widget 2 -->
+            <div class="card">
+                <div class="card-body">
+                    C2
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <!-- Card or widget 3 -->
+            <div class="card">
+                <div class="card-body">
+                    C3
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
