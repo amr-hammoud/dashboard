199afb81469c373f647215c6585b3cd30badc25b
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-10-08 13:10:48 +0300

    implemented the drag-drop feature

---
+++ resources/views/components/parent-box.blade.php
@@ -0,0 +1,4 @@
+<div class="parent flex w-full min-w-[300px] h-fit
+    rounded-lg border border-neutral-300">
+    {{ $slot }}
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Your Laravel App')</title>
+    <title>Dashboard.io</title>
     @vite('resources/css/app.css')
 </head>
 
-<body>
+<body class="h-screen w-full">
     <header>
         <nav>
             <!-- Your navigation menu goes here -->
         </nav>
     </header>
 
-    <main>
+    <main class="h-full w-screen">
         @yield('content')
     </main>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    @vite('resources/js/app.js')
 </body>
 
 </html>
