35e201ec2c3144525b2deb4418bdf92a3b075dc6
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-10-08 23:37:11 +0300

    created the header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,14 +8,19 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="h-screen w-full">
+<body class="h-full w-full">
     <header>
-        <nav>
-            <!-- Your navigation menu goes here -->
-        </nav>
+        <div class="bg-primary-700 h-fit px-5 py-5">
+            <div class="flex flex-wrap content-center gap-3">
+                <div class="flex flex-wrap content-center">
+                    <img class="h-6" src="{{ asset('icons/logo_white.svg') }}" alt="logo" />
+                </div>
+                <div class="flex flex-wrap content-center text-3xl text-neutral-50 font-coolvetica">Dashboard.io</div>
+            </div>
+        </div>
     </header>
 
-    <main class="h-full w-screen">
+    <main class="h-full w-full">
         @yield('content')
     </main>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-    fontFamily: {
-			figtree: ['Figtree', "sans-serif"],
-		},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+        fontFamily: {
+            figtree: ["Figtree", "sans-serif"],
+            coolvetica: ["Coolvetica","sans-serif"],
+        },
+        colors: {
+            white: colors.white,
+            neutral: colors.neutral,
+            primary: {
+                50: "#effaff",
+                100: "#daf3ff",
+                200: "#beebff",
+                300: "#91dfff",
+                400: "#5ecbfc",
+                500: "#38aff9",
+                600: "#2293ee",
+                700: "#1976d2",
+                800: "#1c63b1",
+                900: "#1c548c",
+                950: "#163355",
+            },
+        },
+    },
+    plugins: [],
+};
